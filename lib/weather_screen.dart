3a9345f457fbd07252852e461b106870c543754d
farhanmahmud21 <mahmudkhanfarhan2001@gmail.com>
2025-06-22 01:24:57 +0600

    Current Weather Card created

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -10,6 +10,48 @@ class WeatherScreen extends StatelessWidget {
         title: Text('Weather App'),
         actions: [GestureDetector(child: Icon(Icons.refresh))],
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                shadowColor: Colors.white,
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(14),
+                ),
+                child: Column(
+                  spacing: 10,
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '300.67° F ',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Icon(Icons.cloud, size: 50),
+                    SizedBox(height: 10),
+                    Text(
+                      'Cloudy',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
